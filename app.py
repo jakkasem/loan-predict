36023f415d4eab525a4c177c651e9d942c2049c7
dospoc2023 <dospoc2023@gmail.com>
2026-04-24 16:21:49 +0700

    Fix import statement formatting in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import joblib
 import pyodbc
 import urllib
-import os
+import os  
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split 
